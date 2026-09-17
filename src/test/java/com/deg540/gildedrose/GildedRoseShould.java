5b74ba3b1d8f434a02def4228be75303a525cb21
lilivan11 <ivan.espinosa@540deg.com>
2026-09-17 16:54:33 +0200

    [#5] - Develop tests to general cases (#16)
    * [#5] - Created the enviorement to create tests
    
    * [#5] - Named gildedRose object correctly
    
    * [#5] - Created all general cases tests

--- src/test/java/com/deg540/gildedrose/GildedRoseShould.java
+++ src/test/java/com/deg540/gildedrose/GildedRoseShould.java
@@ -2,12 +2,50 @@ package com.deg540.gildedrose;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.util.List;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 public class GildedRoseShould {
 
     @Test
-    public void tell_the_truth() {
-        assertTrue(true);
+    public void lowerDaysToSellByOneInGeneralItem() {
+        List<Item> items = Arrays.asList(new Item("+5 Dexterity Vest", 10, 20));
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(9, items.get(0).getSellIn());
+    }
+
+    @Test
+    public void lowerQualityByOneForAGeneralItemBeforeSellDate() {
+        List<Item> items = Arrays.asList(new Item("+5 Dexterity Vest", 10, 20));
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(19, items.get(0).getQuality());
+    }
+
+    @Test
+    public void lowerQualityTwiceAsFastForAGeneralItemAfterSellDate() {
+        List<Item> items = Arrays.asList(new Item("+5 Dexterity Vest", 0, 20));
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(18, items.get(0).getQuality());
+    }
+
+    @Test
+    public void neverLowerQualityBelowZero() {
+        List<Item> items = Arrays.asList(new Item("+5 Dexterity Vest", 5, 0));
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(0, items.get(0).getQuality());
     }
 }
