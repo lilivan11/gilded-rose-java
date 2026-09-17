4a093885c12e98cac0854d717fde25b4f4aa3b85
Ivan <ivan.espinosa@540deg.com>
2026-09-17 12:15:13 +0200

    Initial commit

---
+++ src/test/java/com/deg540/gildedrose/GildedRoseShould.java
@@ -0,0 +1,13 @@
+package com.deg540.gildedrose;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class GildedRoseShould {
+
+    @Test
+    public void tell_the_truth() {
+        assertTrue(true);
+    }
+}
